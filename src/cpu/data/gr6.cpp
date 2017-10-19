57af4b14a1d2656f94f7eb3c925fd39eb3193a8c
Max <chistov.m.a@gmail.com>
2017-10-19 23:54:52 +0400

    убрана вибрация передаваемых показаний. исправлен порядок хаписи массивов заданий 2ч

--- src/cpu/data/gr6.cpp
+++ src/cpu/data/gr6.cpp
@@ -9,8 +9,8 @@ namespace data
 {
 wchar_t const* Gr6Pos1Adresses[ GR6::ARRAYS_COUNT ] =
 {
-   L"S7:[S7 connection_4]DB23,REAL2,100",
-   L"S7:[S7 connection_4]DB23,REAL402,100"
+   L"S7:[S7 connection_4]DB23,REAL402,100",
+   L"S7:[S7 connection_4]DB23,REAL2,100"
 };
 wchar_t const* Gr6Pos1Name = L"Gr6Pos1";
 
