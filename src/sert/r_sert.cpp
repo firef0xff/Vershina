3a8060c2e34dda9708079a954a41f7ba81f8ea2a
Chistov Maksim <chistov.m.a@gmail.com>
2017-12-10 10:55:02 +0300

    заменил абсолютные погрешности на относительные
    1ч

--- src/sert/r_sert.cpp
+++ src/sert/r_sert.cpp
@@ -33,20 +33,20 @@ void RSert::PrintProtocol( std::string const& fn)
    fprintf(fprint, "%s  Стенд %s ПОЗ. %s\n\n",dt::ToString( dt::Now() ).c_str(), STAND_NO, mPos.c_str());
    fprintf(fprint, "%s","Определение абсолютной погрешности измерения\n");
    fprintf(fprint, "%s","динамического радиуса\n\n");
-   fprintf(fprint, "%s","+----+-------------+-------------+-----------+\n");
-   fprintf(fprint, "%s","| №  |   значение  |   значение  |абсолютная |\n");
-   fprintf(fprint, "%s","|    |динамического|динамического|погрешность|\n");
-   fprintf(fprint, "%s","|    |  радиуса на |радиуса, из- |     мм    |\n");
-   fprintf(fprint, "%s","|    |   мониторе  |меренное нут-|           |\n");
-   fprintf(fprint, "%s","|    |  стенда, мм |ромером, мм  |           |\n");
-   fprintf(fprint, "%s","+----+-------------+-------------+-----------+\n");
+   fprintf(fprint, "%s","+----+-------------+-------------+-------------+\n");
+   fprintf(fprint, "%s","| №  |   значение  |   значение  |относительная|\n");
+   fprintf(fprint, "%s","|    |динамического|динамического|погрешность  |\n");
+   fprintf(fprint, "%s","|    |  радиуса на |радиуса, из- |     %       |\n");
+   fprintf(fprint, "%s","|    |   мониторе  |меренное нут-|             |\n");
+   fprintf(fprint, "%s","|    |  стенда, мм |ромером, мм  |             |\n");
+   fprintf(fprint, "%s","+----+-------------+-------------+-------------+\n");
 
    for (int i = 0; i < ITEMS_COUNT; i++)
    {
-      fprintf(fprint, "| %2d |   %6.2f    |    %6.2f   |  %7.2f  |\n", i + 1,
+      fprintf(fprint, "| %2d |   %6.2f    |    %6.2f   |   %7.2f   |\n", i + 1,
          ReadoutR[i], MeasuredR[i], RelError[i]);
    }
-   fprintf(fprint, "%s","+----+-------------+-------------+-----------+\n");
+   fprintf(fprint, "%s","+----+-------------+-------------+-------------+\n");
    fclose(fprint);
 }
 
--- src/sert/t_sert.cpp
+++ src/sert/t_sert.cpp
@@ -32,20 +32,20 @@ void TSert::PrintProtocol(std::string const& fn)
    fprintf(fprint, "%s  Стенд %s ПОЗ. %s\n\n", dt::ToString( dt::Now() ).c_str(), STAND_NO ,mPos.c_str());
    fprintf(fprint, "%s","Определение абсолютной погрешности измерения\n");
    fprintf(fprint, "%s","температуры окружающего воздуха\n\n");
-   fprintf(fprint, "%s","+----+------------+-----------+-----------+\n");
-   fprintf(fprint, "%s","| №  |температура,| показания |абсолютная |\n");
-   fprintf(fprint, "%s","|    |измеренная  |на мониторе|погрешность|\n");
-   fprintf(fprint, "%s","|    |термометром |     С     |    С      |\n");
-   fprintf(fprint, "%s","+----+------------+-----------+-----------+\n");
+   fprintf(fprint, "%s","+----+------------+-----------+--------------+\n");
+   fprintf(fprint, "%s","| №  |температура,| показания |относительная |\n");
+   fprintf(fprint, "%s","|    |измеренная  |на мониторе|погрешность, %|\n");
+   fprintf(fprint, "%s","|    |термометром |     С     |              |\n");
+   fprintf(fprint, "%s","+----+------------+-----------+--------------+\n");
 
    for (int i = 0; i < ITEMS_COUNT; i++)
    {
       if (ReadoutT[i] == 0.0)
          break;
-      fprintf(fprint, "| %2d |   %6.2f   |   %6.2f  |  %7.2f  |\n", i + 1,
+      fprintf(fprint, "| %2d |   %6.2f   |   %6.2f  |  %7.2f     |\n", i + 1,
          ReadoutT[i], MeasuredT[i], RelError[i]);
    }
-   fprintf(fprint, "%s","+----+------------+-----------+-----------+\n");
+   fprintf(fprint, "%s","+----+------------+-----------+--------------+\n");
    fclose(fprint);
 }
 }
