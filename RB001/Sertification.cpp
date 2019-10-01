c27418315b90145a2e46a21bc87c0be453481664
m.a. chistov <chistov.m.a@gmail.com>
2019-10-01 22:54:45 +0400

    индексы таблицы

--- RB001/Sertification.cpp
+++ RB001/Sertification.cpp
@@ -210,26 +210,26 @@ __fastcall LCalibr::LCalibr(void)                  // конструктор
 {
   loaded=false;
   Index=0;                            // текущий индекс таблицы
-  TargetLd[0]=5;
-  TargetLd[1]=6;
-  TargetLd[2]=7;
-  TargetLd[3]=8;
-  TargetLd[4]=9;
-  TargetLd[5]=10;
-  TargetLd[6]=11;
-  TargetLd[7]=12;
-  TargetLd[8]=13;
-  TargetLd[9]=15;
-  TargetLd[10]=20;
-  TargetLd[11]=30;
-  TargetLd[12]=40;
+  TargetLd[0]=10;
+  TargetLd[1]=13;
+  TargetLd[2]=16;
+  TargetLd[3]=20;
+  TargetLd[4]=23;
+  TargetLd[5]=26;
+  TargetLd[6]=29;
+  TargetLd[7]=32;
+  TargetLd[8]=35;
+  TargetLd[9]=38;
+  TargetLd[10]=41;
+  TargetLd[11]=44;
+  TargetLd[12]=47;
   TargetLd[13]=50;
-  TargetLd[14]=60;
-  TargetLd[15]=70;
-  TargetLd[16]=80;
-  TargetLd[17]=90;
-  TargetLd[18]=100;
-  TargetLd[19]=110;
+  TargetLd[14]=53;
+  TargetLd[15]=56;
+  TargetLd[16]=60;
+  TargetLd[17]=63;
+  TargetLd[18]=66;
+  TargetLd[19]=70;
   for (int i=20,j=2;i<LDCQTY;i++) {    // столбец заданных нагрузок
     TargetLd[i]=TargetLd[20-(j++)];
   }
