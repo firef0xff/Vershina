be00bbdddd03efd5cc2109f15e960a54fff056de
Chistov M.A <chistov.m.a@gmail.com>
2019-10-02 14:19:01 +0400

    lost files

---
+++ RB001/ABOUT.h
@@ -0,0 +1,32 @@
+//----------------------------------------------------------------------------
+#ifndef AboutH
+#define AboutH
+//----------------------------------------------------------------------------
+#include <System.hpp>
+#include <Windows.hpp>
+#include <SysUtils.hpp>
+#include <Classes.hpp>
+#include <Graphics.hpp>
+#include <Forms.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Buttons.hpp>
+#include <ExtCtrls.hpp>
+//----------------------------------------------------------------------------
+class TAboutBox : public TForm
+{
+__published:
+	TPanel *Panel1;
+	TImage *ProgramIcon;
+	TLabel *ProductName;
+	TLabel *Copyright;
+	TLabel *Comments;
+	TButton *OKButton;
+private:
+public:
+	virtual __fastcall TAboutBox(TComponent* AOwner);
+};
+//----------------------------------------------------------------------------
+extern PACKAGE TAboutBox *AboutBox;
+//----------------------------------------------------------------------------
+#endif    
